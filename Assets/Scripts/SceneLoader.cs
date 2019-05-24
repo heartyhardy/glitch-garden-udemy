22dfdcadc833b5dc4eeb107d710c54b1fdb1fa85
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-05-24 17:13:28 +0530

    First commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    int currentBuildIndex = 0;
+
+    private void Start()
+    {
+        currentBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentBuildIndex <= 0)
+            ++currentBuildIndex;
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(++currentBuildIndex);
+    }
+}
---
+++ Assets/Scripts/StartMenuLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenuLoader : MonoBehaviour {
+
+    private const float LOAD_DELAY = 3f;
+
+    IEnumerator Start()
+    {
+        Cursor.visible = false;
+        yield return new WaitForSeconds(LOAD_DELAY);
+        SceneManager.LoadScene("StartMenu");
+        Cursor.visible = true;
+    }
+}
